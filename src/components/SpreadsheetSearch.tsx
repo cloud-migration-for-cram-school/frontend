a28159ecfbd845f69d3fe447ed6b7e97809571f1
wsdnakamoto <wsdnakamoto@gmail.com>
2024-08-25 23:56:47 +0900

    fix: 検索に失敗したときのエラーメッセージを表示, エラーメッセージでフォームの位置がずれないように修正.

--- src/components/SpreadsheetSearch.tsx
+++ src/components/SpreadsheetSearch.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useForm, Controller, SubmitHandler } from "react-hook-form";
-import { HStack, Autocomplete, FormControl, Button } from "@yamada-ui/react";
+import { HStack, Autocomplete, FormControl, Button, ErrorMessage, Alert, AlertDescription } from "@yamada-ui/react";
 import { Spreadsheet, Subject, FormData } from "../types/SpreadsheetSearch";
 import dummySheetData from "../assets/dummySheetData";
 import dummySheets from "../assets/dummySheets";
@@ -13,6 +13,7 @@ const SpreadsheetSearch = () => {
   const [sheets, setSheets] = useState<Spreadsheet[]>([]);
   const [subjects, setSubjects] = useState<Subject[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [isInvalid, setIsInvalid] = useState(false);
   const navigate = useNavigate();
   const {
     control,
@@ -49,6 +50,7 @@ const SpreadsheetSearch = () => {
 
   const fetchReportBySubject = async (sheet_id: string, subjects_id: string) => {
     setIsLoading(true);
+    setIsInvalid(false);
     try {
       const response = await axios.get<SheetData>(
         `http://~:8080/search/subjects/reports/${sheet_id}/${subjects_id}`
@@ -58,6 +60,7 @@ const SpreadsheetSearch = () => {
       });
     } catch (error) {
       console.error("エラーが発生しました:", error);
+      setIsInvalid(true);
     } finally {
       setIsLoading(false);
     }
@@ -73,77 +76,93 @@ const SpreadsheetSearch = () => {
 
   return (
     <div>
-      <HStack as="form" onSubmit={handleSubmit(onSubmit)}>
-        <FormControl
-          isInvalid={!!errors.selectedSheetId}
-          errorMessage={
-            errors.selectedSheetId ? errors.selectedSheetId.message : undefined
-          }
-        >
-          <Controller
-            key={sheets?.length || "initial"}
-            name="selectedSheetId"
-            control={control}
-            rules={{ required: { value: true, message: "選択してください" } }}
-            render={({ field }) => (
-              <Autocomplete
-                placeholder="スプレッドシートを選択"
-                {...field}
-                items={sheets}
-                closeOnSelect={false}
-                allowFree
-                emptyMessage="存在しません"
-                variant="flushed"
-                iconProps={{ color: "primary" }}
-                size={"lg"}
-                onChange={(value) => {
-                  console.log(value);
-                  field.onChange(value);
-                  onSelectSpreadsheet(value);
-                }}
-              />
-            )}
-          />
-        </FormControl>
-        <FormControl
-          isInvalid={!!errors.selectedSubjectId}
-          errorMessage={
-            errors.selectedSubjectId
-              ? errors.selectedSubjectId.message
-              : undefined
-          }
-        >
-          <Controller
-            key={subjects?.length || "initial"}
-            name="selectedSubjectId"
-            control={control}
-            rules={{ required: { value: true, message: "選択してください" } }}
-            render={({ field }) => (
-              <Autocomplete
-                placeholder="科目を選択"
-                {...field}
-                items={subjects}
-                closeOnSelect={false}
-                allowFree
-                emptyMessage="先にスプレッドシートを選択してください"
-                variant="flushed"
-                iconProps={{ color: "primary" }}
-                size={"lg"}
-              />
-            )}
-          />
-        </FormControl>
-        <Button
-          type="submit"
-          loadingIcon="grid"
-          colorScheme="primary"
-          isLoading={isLoading}
-          variant="ghost"
-          size={"lg"}
-        >
-          検索
-        </Button>
-      </HStack>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <HStack>
+          <FormControl
+            isInvalid={!!errors.selectedSheetId}
+          >
+            <Controller
+              key={sheets?.length || "initial"}
+              name="selectedSheetId"
+              control={control}
+              rules={{ required: { value: true, message: "選択してください" } }}
+              render={({ field }) => (
+                <Autocomplete
+                  placeholder="スプレッドシートを選択"
+                  {...field}
+                  items={sheets}
+                  closeOnSelect={false}
+                  allowFree
+                  emptyMessage="存在しません"
+                  variant="flushed"
+                  iconProps={{ color: "primary" }}
+                  size={"lg"}
+                  onChange={(value) => {
+                    field.onChange(value);
+                    onSelectSpreadsheet(value);
+                  }}
+                />
+              )}
+            />
+            <ErrorMessage p="0px" m="0px">
+              <Alert status="error" variant="island-accent" size="xs" border="none" position="absolute" top="50px">
+                <AlertDescription>
+                  {errors.selectedSheetId ? errors.selectedSheetId.message : undefined}
+                </AlertDescription>
+              </Alert>
+            </ErrorMessage>
+          </FormControl>
+          <FormControl
+            isInvalid={!!errors.selectedSubjectId}
+          >
+            <Controller
+              key={subjects?.length || "initial"}
+              name="selectedSubjectId"
+              control={control}
+              rules={{ required: { value: true, message: "選択してください" } }}
+              render={({ field }) => (
+                <Autocomplete
+                  placeholder="科目を選択"
+                  {...field}
+                  items={subjects}
+                  closeOnSelect={false}
+                  allowFree
+                  emptyMessage="先にスプレッドシートを選択してください"
+                  variant="flushed"
+                  iconProps={{ color: "primary" }}
+                  size={"lg"}
+                />
+              )}
+            />
+            <ErrorMessage p="0px" m="0px">
+              <Alert status="error" variant="island-accent" size="xs" border="none" position="absolute" top="50px">
+                <AlertDescription>
+                  {errors.selectedSubjectId? errors.selectedSubjectId.message : undefined}
+                </AlertDescription>
+              </Alert>
+            </ErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={isInvalid}>
+            <Button
+              type="submit"
+              loadingIcon="grid"
+              colorScheme="primary"
+              isLoading={isLoading}
+              variant="ghost"
+              size="lg"
+            >
+              検索
+            </Button>
+            <ErrorMessage p="0px" m="0px">
+              <Alert status="error" variant="island-accent" size="xs" border="none" position="absolute" top="50px">
+                <AlertDescription>
+                  検索に失敗しました
+                </AlertDescription>
+              </Alert>
+            </ErrorMessage>
+          </FormControl>
+        </HStack>
+      </form>
     </div>
   );
 };
